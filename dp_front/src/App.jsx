abf2cd67c1eca520e6b40ec45cf8cdb608fe7549
Harshit Bajpai <rogeralpha700@gmail.com>
2023-11-24 16:57:45 +0530

    removed chatbot

--- dp_front/src/App.jsx
+++ dp_front/src/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import Chatbot from 'react-chatbot-kit'
-import Config from './Components/Chatbot/chatbotConfig'
-import ActionProvider from './Components/Chatbot/ActionProvider'
-import MessageParser from './Components/Chatbot/MessageParser'
-import 'react-chatbot-kit/build/main.css';
 
 const App = () => {
   return (
     <div>
-      <Chatbot
-        config= { Config }
-        messageParser = { MessageParser }
-        actionProvider= { ActionProvider }
-      />
     </div>
   )
 }
