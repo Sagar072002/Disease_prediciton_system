97b1f23ca9318d3f8686238de0e55aa31dcf454c
Sagar Negi <negis2673@gmail.com>
2023-11-27 15:03:37 +0530

    added counter,faq and review component

---
+++ dp_front/src/Components/Review/Review.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import './Review.css';
+import img from '../../assets/sekiro.jpg'
+
+
+const Review = () => {
+  return (
+    <div className='review-section'>
+        <h1>Customer Review</h1>
+        <p className='text'>Read what our customers have to say about us</p>
+        <div className="inner-review">
+        <div className="review">
+            <div className="top-section">
+            <img src={img} alt="" />
+            <div className="detail">
+            <h2>Sagar Negi</h2>
+            <p>Position,Company</p>
+            </div>
+            
+            </div>
+            <div className="stars">
+            <i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-regular fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa repudiandae nisi ut dolor alias a maxime aliquam omnis aspernatur sunt eaque exercitationem rerum, assumenda velit eveniet facere. Ipsum, soluta.</p>
+        </div>
+        <div className="review">
+        <div className="top-section">
+            <img src={img} alt="" />
+            <div className="detail">
+            <h2>Sagar Negi</h2>
+            <p>Position,Company</p>
+            </div>
+            </div>
+            <div className="stars">
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-regular fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa repudiandae nisi ut dolor alias a maxime aliquam omnis aspernatur sunt eaque exercitationem rerum, assumenda velit eveniet facere. Ipsum, soluta.</p>
+        </div>
+        <div className="review">
+        <div className="top-section">
+            <img src={img} alt="" />
+            <div className="detail">
+            <h2>Sagar Negi</h2>
+            <p>Position,Company</p>
+            </div>
+            </div>
+            <div className="stars">
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-regular fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa repudiandae nisi ut dolor alias a maxime aliquam omnis aspernatur sunt eaque exercitationem rerum, assumenda velit eveniet facere. Ipsum, soluta.</p>
+        </div>
+        </div>
+       
+        <div className="inner-review">
+          <div className="review">
+          <div className="top-section">
+            <img src={img} alt="" />
+            <div className="detail">
+            <h2>Sagar Negi</h2>
+            <p>Position,Company</p>
+            </div>
+            </div>
+            <div className="stars">
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa repudiandae nisi ut dolor alias a maxime aliquam omnis aspernatur sunt eaque exercitationem rerum, assumenda velit eveniet facere. Ipsum, soluta.</p>
+        </div>
+        <div className="review">
+        <div className="top-section">
+            <img src={img} alt="" />
+            <div className="detail">
+            <h2>Sagar Negi</h2>
+            <p>Position,Company</p>
+            </div>
+            </div>
+            <div className="stars">
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa repudiandae nisi ut dolor alias a maxime aliquam omnis aspernatur sunt eaque exercitationem rerum, assumenda velit eveniet facere. Ipsum, soluta.</p>
+        </div>
+        <div className="review">
+        <div className="top-section">
+            <img src={img} alt="" />
+            <div className="detail">
+            <h2>Sagar Negi</h2>
+            <p>Position,Company</p>
+            </div>
+            </div>
+            <div className="stars">
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-solid fa-star"></i>
+<i class="fa-regular fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa repudiandae nisi ut dolor alias a maxime aliquam omnis aspernatur sunt eaque exercitationem rerum, assumenda velit eveniet facere. Ipsum, soluta.</p>
+        </div>
+        
+        </div>
+        
+       
+        
+    </div>
+  )
+}
+
+export default Review
