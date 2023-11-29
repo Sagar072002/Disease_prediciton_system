31662628fd35966011661b7c290c28a1fcf59f76
Sagar Negi <negis2673@gmail.com>
2023-11-29 16:15:45 +0530

    changed header

--- dp_front/src/Components/Header/Header.jsx
+++ dp_front/src/Components/Header/Header.jsx
@@ -9,20 +9,18 @@ const Header = () => {
     <img src={img} alt="" />
     <input className="searchbar" type="search" name="" placeholder='Search here...' id="" />
     <div className="links">
+    <a className="active" href="#home">Home</a>
     <Link className="active" to="/">Home</Link>
     <a href="#">About</a>
     <a href="#">Prediction</a>
     <a href="#">Appointment</a>
+    <a href="#">Contact</a>
+    <a href="#">Login</a>
     <Link to="/contact">Contact</Link>
     <Link to="/login">Login</Link>
     {/* <a href="#">Register</a> */}
     </div>
   </div>
-  )
-}
-
-export default Header
-
-
-
-
+   )
+  }
+  export default Header
